6bd10dc061154b58d12e3f798095e09043e725ae
Nate Ventrella <nateven@gmail.com>
2022-08-09 12:45:23 -0400

    Co-authored-by: Jared <jaredKrajewski@users.noreply.github.com>

--- warmup2.js
+++ warmup2.js
@@ -2,7 +2,7 @@
 Goals: return 'n' copies of given string;
 Params: String1: str, String2: newStr, Number1: i;
 Return Val: String;
-Logic:  
+Logic: return str, repeated 'n' times
 
 Given a string and a non-negative int n, return a larger string that is n copies of the original string.
 
@@ -11,6 +11,124 @@ stringTimes("Hi", 3) → "HiHiHi"
 stringTimes("Hi", 1) → "Hi"
 */
 
-stringTimes("Hi", 2) → "HiHi"
-stringTimes("Hi", 3) → "HiHiHi"
-stringTimes("Hi", 1) → "Hi"
+const stringTimes = (str, i) => str.repeat(i);
+
+
+console.log(stringTimes("Hi", 2));
+console.log(stringTimes("Hi", 3));
+console.log(stringTimes("Hi", 1));
+
+/*
+Goals: return a given number copies of a given string's first 3 chars;
+Params: String1: str, Number1: i;
+Return Val: String;
+Logic: if str length < 3, return 'i' copies of str. otherwise return 'i' copies of str's first 3 chars
+
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+frontTimes("Chocolate", 2) → "ChoCho"
+frontTimes("Chocolate", 3) → "ChoChoCho"
+frontTimes("Abc", 3) → "AbcAbcAbc"
+*/
+
+const frontTimes = (str, i) => str.length < 3 ? str.substring(0).repeat(i)
+    : str.substr(0, 3).repeat(i);
+
+console.log(frontTimes("Chocolate", 2));
+console.log(frontTimes("Chocolate", 3));
+console.log(frontTimes("Abc", 3));
+console.log(frontTimes("Ab", 3));
+
+/*
+Goals: count and return total number of "xx" occurances in given string;
+Params: String1: str, Number1: count, Number2: i;
+Return Val: Number;
+Logic: create a variable named count
+using a for loop, check 'str' for "xx" moving one char at a time.
+if 
+
+Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+countXX("abcxx") → 1
+countXX("xxx") → 2
+countXX("xxxx") → 3
+*/
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
+
+
+/*
+Goals: 
+Params: 
+Return Val: 
+Logic: 
+*/
